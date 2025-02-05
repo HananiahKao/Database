5ea840a204e6ab9014f7c01d4d1d4b4c5d91d46a
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-02-05 19:38:52 +0800

    use git config to configurate user name and email before committing.

--- commitDB.sh
+++ commitDB.sh
@@ -1,3 +1,5 @@
 git add .
+git config --global user.email "HananiahKao@users.noreply.github.com"
+git config --global user.name "HananiahKao"
 git commit -m"state of database at $(date "+%Y/%m/%d %H:%M:%S")"
 git push -u origin main
